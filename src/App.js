57606ee6a1a5dd9cbe86b2e561df031dccc66c60
MashaZenchenkova <zenchenkova.m@mail.ru>
2026-06-23 15:32:36 +0300

    Добавлен слайдер

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Button from './components/Button';
-import Perecl from './components/Perecl'
+import Perecl from './components/Perecl';
+import Slider from './components/Slider'; 
 import './App.css';
 
 function App() {
@@ -7,8 +8,8 @@ function App() {
     <div className="app-container">
       <Button />
       <Perecl />
+      <Slider label="Не знаю еще что" min={0} max={100} initialValue={50} />
     </div>
   );
 }
-
 export default App;
